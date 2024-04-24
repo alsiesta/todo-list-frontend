badc8a3a4aec85f4e1725143839c97150ff70da8
alsiesta <as@daslizenzatelier.de>
2024-04-24 23:49:21 +0200

    created login()

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,14 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  email: string = '';
+  username: string = '';
   password: string = '';
 
-  login() {
-    // logik Backend anfragen
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    const raw = JSON.stringify({
+      username: this.username,
+      password: this.password,
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    try{
+    let response = await fetch('http://127.0.0.1:8000/login/', requestOptions)
+    let json = await response.json() 
+      localStorage.setItem('token', json.token)
+      console.log(response.body)
+      // Redirect to the home page
+    } catch (error) {
+      // Handle the error
+      console.error('Error:', error);
+    }
+
   }
 
+  ngOnInit(): void {}
 }
