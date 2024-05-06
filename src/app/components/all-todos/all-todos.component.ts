b1d4e390ac138ec374b522f53cebea57291e0845
alsiesta <as@daslizenzatelier.de>
2024-05-06 17:13:20 +0200

    added update and create new tasks method

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -1,10 +1,74 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { HttpHeaders } from '@azure/storage-blob';
+import { lastValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-all-todos',
   templateUrl: './all-todos.component.html',
   styleUrls: ['./all-todos.component.scss']
 })
+
 export class AllTodosComponent {
+  todos: any = [];
+  error = '';
+  newTodo = '';
+
+addTodo() {
+  const url = environment.baseUrl + '/todos/';
+  const user = localStorage.getItem('user_id');
+  const body = { author: user, title: this.newTodo };
+  console.log(body);
+
+  this.http.post(url, body).subscribe(
+    response => {
+      console.log(response);
+      this.loadTodos().then(todos => {
+        this.todos = todos;
+      } 
+      );
+    },
+    error => {
+      console.error('Error:', error);
+    }
+  );
+}
+  
+onCheckboxChange(event: any, todo: any) {
+  const url = environment.baseUrl + '/todos/' + todo.id + '/';
+  const updatedTodo = { ...todo, checked: event.target.checked };
+
+  this.http.put(url, updatedTodo).subscribe(
+    response => {
+      console.log(response);
+      this.loadTodos().then(todos => {
+        this.todos = todos;
+      });
+    },
+    error => {
+      console.error('Error:', error);
+    }
+  );
+}
+
+  constructor (private http: HttpClient) { }
+
+  async ngOnInit () {
+    try {
+      this.todos = await this.loadTodos();
+      console.log(this.todos);
+    } catch (e) {
+      this.error = 'Fehler beim laden';
+      console.error('Error:', e);
+    }
+  }
+
+  loadTodos () {
+    const url = environment.baseUrl + '/todos/';
+    return lastValueFrom(this.http.get(url)
+    );
+  }
 
 }
+
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +14,7 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private http:HttpClient, private auth: AuthService) {}
+  constructor(private http:HttpClient, private auth: AuthService, private router: Router) {}
 
   async login () {
     // DAS ALLES KANN WEGGELASSEN WERDEN; WEIL WIR DAS JETZT IN EINER FUNKTION AUSGELAGERT HABEN (loginWithUsernameAndPassword)
@@ -32,8 +34,11 @@ export class LoginComponent {
     // };
 
     try {
-      let response = await this.auth.loginWithUsernameAndPassword(this.username, this.password);
+      let response:any = await this.auth.loginWithUsernameAndPassword(this.username, this.password);
       console.log(response);
+      localStorage.setItem('token', response.token);
+      localStorage.setItem('user_id', response.user_id);
+      this.router.navigate(['/todos']);
 
     // let response = await fetch(environment.baseUrl + '/login/', requestOptions)
     // let json = await response.json()
@@ -42,6 +47,7 @@ export class LoginComponent {
     // Redirect to the home page
       
     } catch (error) {
+      alert('Login failed');
       console.error('Error:', error);
     }
 
